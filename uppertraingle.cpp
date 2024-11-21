471b3791a8668825ef0b92de986414f8560b4396
sharad8979 <sharadsharma8439@gmail.com>
2024-11-21 05:50:02 +0530

    Add files via upload

---
+++ uppertraingle.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int arr[3][3];
+   cout<<"Enter the elements in Array";
+   for(int i=0;i<3;i++){
+     for(int j=0;j<3;j++){
+        cin>>arr[i][j];
+     }
+   }
+   cout<<"\n Array is=\n";
+   for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        cout<<" "<<arr[i][j];
+    }
+    cout<<"\n";
+   }
+
+   for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+         if(i>j){
+            arr[i][j]=0;
+         }
+    }
+   }
+        cout<<"\nUpper traingular matrix is=\n";
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                cout<<" "<<arr[i][j];
+            }
+            cout<<endl;
+        }
+
+    return 0;
+}
